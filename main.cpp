0ab6c2554472756554b1f6f95df6e0930dbdf104
CarlosEZaul <carlosezzddomingos@gmail.com>
2025-04-28 20:19:07 -0300

    projeto

--- main.cpp
+++ main.cpp
@@ -1,1 +1,58 @@
-aa
+#include <iostream>
+
+using namespace std;
+
+struct Tipo_Produtos{
+	int codigo;
+	string descricao;
+};
+
+void ler_tipo_produtos(Tipo_Produtos lista[]){
+	system("cls");
+	
+}
+
+struct Produtos{
+	int codigo;
+	string descricao;
+	Tipo_Produtos tipo_produto;
+	int codigo_fornecedor;
+	int qtd_estoque;
+	int estoque_minimo;
+	int estoque_maximo;
+	float valor_unitario;
+};
+
+void ler_produtos(Produtos lista[]){
+	system("cls");
+	
+}
+
+struct Cidades{
+	int codigo;
+	string descricao;
+	char UF[2];
+};
+
+void ler_cidades(Cidades lista[]){
+	system("cls");
+	
+}
+
+struct Fornecedores{
+	int codigo;
+	string nome;
+	string endereco;
+	string telefone;
+	int codigo_cidade;
+	string cnpj;
+};
+
+void ler_fornecedores(Fornecedores lista[]){
+	system("cls");
+	
+}
+
+int main(){
+	
+}
